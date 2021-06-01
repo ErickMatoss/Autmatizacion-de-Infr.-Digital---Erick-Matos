6a676b482b6c20ba8ba06eb4dc31c38ecbb8bf21
Erick Javier Matos Ramos <57080816+ErickMatoss@users.noreply.github.com>
2021-06-01 17:33:37 -0500

    Delete LlamadoDeFunciones.py

--- LlamadoDeFunciones.py
+++
@@ -1,9 +0,0 @@
-#llamamos las funciones declaradas en otro archivo
-from FuncionesExternas import *
-
-#invocamos las funciones
-funcion1()
-#invocamos la otra funcion
-Q=input('Introduzca un dato: ')
-W=input('Introduzca otro dato: ')
-print(funcion2(Q,W))
