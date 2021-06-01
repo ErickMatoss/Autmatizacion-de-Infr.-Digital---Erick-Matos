44ba3e75627becf4142ff870eed333919ed28dd6
Erick Javier Matos Ramos <57080816+ErickMatoss@users.noreply.github.com>
2021-06-01 17:16:08 -0500

    creacion de funcionesexternas.py

---
+++ FuncionesExternas.py
@@ -0,0 +1,11 @@
+#Declaracion de funciones
+
+#Funcion sin parametros
+def funcion1():
+    #Contenido de la funcion
+    print('Has llamado a la funcion')
+#funcion con parametros y retorno
+def funcion2 (a,b):
+    #contenido de la funcion
+    c=int(a)+int(b)
+    return c
